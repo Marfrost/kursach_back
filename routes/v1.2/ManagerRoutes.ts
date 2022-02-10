4e39ee4f2ca0d42eea3f64147bd9b780f1f10ac3
Volodymyr Ryzhkov <waryzhkov@gmail.com>
2022-02-10 22:05:23 +0200

    Changed auth on manager/all

--- routes/v1.2/ManagerRoutes.ts
+++ routes/v1.2/ManagerRoutes.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import ManagerController from "../../controllers_v1.2/ManagerController";
 import authMiddleware from "../../middleware/authMiddleware"
 const router = Router();
+router.get("/all", ManagerController.get)
 
 router.use(authMiddleware);
 router.post('',   ManagerController.create)
 router.delete('', ManagerController.delete)
 router.put('', ManagerController.update)
-router.get("/all", ManagerController.get)
 router.get("/", ManagerController.getById)
 
 
